79b8a7aea5ace661c461c40b16ccc07521cef770
saurabh <ss12saurav@gmail.com>
2025-02-05 15:27:55 +0530

    latest changes

--- FoodMenu/food/models.py
+++ FoodMenu/food/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Item(models.Model):
+    def __str__(self):
+        return self.item_name
+
     item_name = models.CharField(max_length=200)
-    Item_desc = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://blocks.astratic.com/img/general-img-portrait.png")
--- FoodMenu/food/urls.py
+++ FoodMenu/food/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'food'
 urlpatterns = [
-    path('', views.index),
-    path('item/', views.item),
+    path('', views.index, name="index"),
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
+    path('add/', views.create_item, name='add_item'),
 ]
